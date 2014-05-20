0b1b00aede35b5ab3c7f3ce788a4f972cf7feef0
Keely Patrick <keely.faye@gmail.com>
2014-05-20 12:14:37 -0500

    Updating recipe with kafka8 info.

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,9 +5,9 @@
 
 case kafka_install_method
 when :source, :binary
-  include_recipe 'kafka::_setup'
-  include_recipe %(kafka::#{node[:kafka][:install_method]})
-  include_recipe 'kafka::_configure'
+  include_recipe 'kafka8::_setup'
+  include_recipe %(kafka8::#{node[:kafka][:install_method]})
+  include_recipe 'kafka8::_configure'
 else
   Chef::Application.fatal! %(Unknown install_method: #{node[:kafka][:install_method].inspect})
 end
